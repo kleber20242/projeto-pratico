98fa4c3b6785b6a09a7ad3e4c9c0c72c95e7baf6
kleber20242 <gudezinn@gmail.com>
2024-08-27 19:37:27 -0300

    versao final

---
+++ index.js
@@ -0,0 +1,9 @@
+var titulocentral = window.document.getElementById("titulocentral")
+var textocentral = window.document.getElementById("textocentral")
+
+
+
+function ocultar() {
+    textocentral.style ="opacity: 100; transition-duration: 2s; "
+}
+
